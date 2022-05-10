cd6d6f5b05c4c28fc12058d54145b432f8ae8cfe
alfinarisky <76677443+alfinarisky@users.noreply.github.com>
2022-05-11 05:22:35 +0700

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -142,7 +142,7 @@ def get_readable_message():
                     msg += f"\n<b>🔺 Uploaded:</b> <code>{get_readable_file_size(download.processed_bytes())}</code> of <code>{download.size()}</code>"
                 else:
                     msg += f"\n<b>🔻 Downloaded:</b> <code>{get_readable_file_size(download.processed_bytes())}</code> of <code>{download.size()}</code>"
-                msg += f"\n<b>📶 Speed:</b> <code>{download.speed()}</code> <b>ETA:</b> <code>{download.eta()}</code>"
+                msg += f"\n<b>🚀 Speed:</b> <code>{download.speed()}</code> <b>⏳ ETA:</b> <code>{download.eta()}</code>"
                 try:
                     msg += f"\n<b>🧲 Seeders:</b> <code>{download.aria_download().num_seeders}</code>" \
                            f" | <b>🛰️ Peers:</b> <code>{download.aria_download().connections}</code>"
